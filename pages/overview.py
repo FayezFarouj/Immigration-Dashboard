6f9e372bf2885ea780efb0e422405b8500f29acf
FayezFarouj <fayez.farouj@gmail.com>
2026-07-24 10:04:45 -0700

    Add Overview Page

---
+++ pages/overview.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+st.title("Immigration Dashboard")
+
+st.write("""
+This project explores international mmigration data using
+interactive dashboards.
+
+Use the navigation on the left to explore the dashboards.
+""")
